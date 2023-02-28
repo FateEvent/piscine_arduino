ea4adcfbfca60a22c776ea52983c7e672e542314
FateEvent <87657843+FateEvent@users.noreply.github.com>
2023-02-28 08:31:08 +0100

    Update sketch_feb26a.ino

--- perColpaDiQualcuno/sketch_feb26a.ino
+++ perColpaDiQualcuno/sketch_feb26a.ino
@@ -12,6 +12,7 @@ void setup(){
 	bool	resistorsOnSegments = true;
 	byte	hardwareConfig = COMMON_CATHODE;
  
+	Serial.begin(9600);
 	sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
 	sevseg.setBrightness(90);
 }
