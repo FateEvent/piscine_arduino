35038f3f3d50cad7e96ba2e58553c1df59c3fbe7
Fab <fabio.venturini.1989@gmail.com>
2023-03-01 23:27:18 +0100

    just 3 numbers are displayed by now

--- perColpaDiQualcuno/README.md
+++ perColpaDiQualcuno/README.md
@@ -4,6 +4,5 @@ To understand the different types of resistor:
 * <https://uk.rs-online.com/web/content/discovery/ideas-and-advice/resistors-guide>.
 
 The tutorials I followed:
-* <https://www.circuitbasics.com/arduino-7-segment-display-tutorial>;
 * <https://arduino-france.site/7-segments>;
 *  <https://www.youtube.com/watch?v=tdexkAEfHg0>.
--- perColpaDiQualcuno/sketch_feb26a.ino
+++ perColpaDiQualcuno/sketch_feb26a.ino
@@ -5,28 +5,49 @@ char	*str = "We are such stuff As dreams are made on, and our little life Is rou
 //char	*str = "Per colpa di qualcuno non si fa credito a nessuno.";
 int		len = strlen(str);
 
-void setup(){
-	byte	numDigits = 1;
-	byte	digitPins[] = {};
-	byte	segmentPins[] = {6, 5, 2, 3, 4, 7, 8, 9};
-	bool	resistorsOnSegments = true;
-	byte	hardwareConfig = COMMON_CATHODE;
+#define A  8
+#define B  7
+#define C  6
+#define D  5
+#define E  4
+#define F  3
+#define G  2
 
-  	Serial.begin(9600);
-	sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
-	sevseg.setBrightness(90);
+void setup() {
+	pinMode(A, OUTPUT);
+	pinMode(B, OUTPUT);
+	pinMode(C, OUTPUT);
+	pinMode(D, OUTPUT);
+	pinMode(E, OUTPUT);
+	pinMode(F, OUTPUT);
+	pinMode(G, OUTPUT);
 }
 
 void loop() {
-	int i = 0;
-
-	while (i < len)
-	{
-		char	buf[2] = {str[i], 0};
-
-		sevseg.setChars(buf);
-		delay(500);
-		sevseg.refreshDisplay();
-		++i;
-	}
+	digitalWrite(A, LOW);
+	digitalWrite(B, HIGH);
+	digitalWrite(C, HIGH);
+	digitalWrite(D, LOW);
+	digitalWrite(E, LOW);
+	digitalWrite(F, LOW);
+	digitalWrite(G, LOW);
+	delay(1000);
+ 
+	digitalWrite(A, HIGH);
+	digitalWrite(B, HIGH);
+	digitalWrite(C, LOW);
+	digitalWrite(D, HIGH);
+	digitalWrite(E, HIGH);
+	digitalWrite(F, LOW);
+	digitalWrite(G, HIGH);
+	delay(1000);
+ 
+	digitalWrite(A, HIGH);
+	digitalWrite(B, HIGH);
+	digitalWrite(C, HIGH);
+	digitalWrite(D, HIGH);
+	digitalWrite(E, LOW);
+	digitalWrite(F, LOW);
+	digitalWrite(G, HIGH);
+	delay(1000);
 }
